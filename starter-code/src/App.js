a2fe1914efcd92b2631d969028f8f6fc9f4f3943
joelmzramos <joel.ramos@accenture.com>
2019-10-30 22:13:34 -0300

    done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -2,17 +2,60 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import foods from './foods.json'
+
+import FoodBox from './components/FoodBox/FoodBox';
+import FormAdd from './components/FormAdd/FormAdd';
+import Search from './components/Search/Search';
+import SelectedFoods from './components/SelectedFoods/SelectedFoods'
+
 class App extends Component {
+  state = {
+    foods: [],
+    filteredFoods: []
+  }
+
+  componentDidMount() {
+    this.setState({
+      foods: foods.slice(0),
+      filteredFoods: foods.slice(0),
+    });
+  }
+
+  addFoodBox = (data) => {
+    const { foods, filteredFoods } = this.state;
+    foods.push(data);
+    filteredFoods.push(data);
+    this.setState({ foods, filteredFoods })
+  }
+
+  filterFoods = (filter) => {
+    const { foods } = this.state;
+    const { name } = filter;
+
+    const filteredFoods = foods.filter((food) => {
+      return food.name.toLowerCase().includes(name);
+    });
+
+    this.setState({
+      filteredFoods,
+    })
+  }
+
   render() {
+    
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <FormAdd funcAddFood={this.addFoodBox} />
+        <Search filterFunction={this.filterFoods}/>
+        <div className="columns">
+          <div className="column">
+            <FoodBox foods={this.state.filteredFoods}/>
+          </div>
+          <div className="column">
+            <SelectedFoods />
+          </div>
+      </div>
       </div>
     );
   }
